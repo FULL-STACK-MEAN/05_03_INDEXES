ecb6e20618adf88a9ae13d22daadaefd76d62f8a
getafemean <getafemean@gmail.com>
2021-07-15 19:00:58 +0200

    employees zoquete!

--- 10_colacion.js
+++ 10_colacion.js
@@ -25,14 +25,14 @@ db.runners.find({surname1: "Lopez"}).collation({locale: 'en', strength: 1}) // c
 // - Si en la consulta se especifica colación y es diferente a la de la colección no
 //   se usará el índice
 
-db.createCollection('employers', {collation: {locale: 'es', strength: 1}})
+db.createCollection('employees', {collation: {locale: 'es', strength: 1}})
 
-db.employers.insert({name: 'Juan', surname: 'Pérez'})
+db.employees.insert({name: 'Juan', surname: 'Pérez'})
 
-db.employers.createIndex({surname: 1})
+db.employees.createIndex({surname: 1})
 
-db.employers.find({surname: 'Perez'}) // Aunque no se indique la colación se usa el índice
+db.employees.find({surname: 'Perez'}) // Aunque no se indique la colación se usa el índice
 // porque la colación esta establecida a nivel de colección
 
-db.employers.find({surname: 'Perez'} ,{collation: {locale: 'en', strength: 1}}) // e Aunque la
+db.employees.find({surname: 'Perez'} ,{collation: {locale: 'en', strength: 1}}) // e Aunque la
 // colección tiene una colación, al especificar una colación diferent no usará el índice
